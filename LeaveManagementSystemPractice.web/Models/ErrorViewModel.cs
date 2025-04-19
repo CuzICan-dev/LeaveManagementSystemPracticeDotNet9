9c4511406102a9959aebda32f7e23d48998e80c1
Roman Bits <romanbits2@gmail.com>
2025-04-19 19:13:55 +0300

    initial commit - project creation

---
+++ LeaveManagementSystemPractice.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeaveManagementSystemPractice.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
